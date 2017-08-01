cbed860f53e3359af3f7cb76b006cc064ac5d664
chiyu1468 <chiyu1468@hotmail.com>
2017-08-01 22:22:55 +0800

    fix for windows can run

--- GetFileName/DirFunc.py
+++ GetFileName/DirFunc.py
@@ -7,12 +7,15 @@ ExFile = ['filename.csv', 'DirFunc.py']
 
 # 要處理的資料夾路徑
 FileDirPath = []
+# 印出用的
+sizeLevel = ("B", "kB" , "MB", "GB", "TB")
 
 def main():
     # 取得當前路徑
-    fpath = os.path.abspath(__file__)
-    for L1 in re.finditer("\/", fpath): pass
-    fpath = fpath[:L1.span(0)[0]]
+    fpath = os.path.abspath(__file__)    
+    L1 = re.match("","")
+    for L1 in re.finditer("[/\\\]", fpath): pass
+    fpath = fpath[:L1.regs[0][0]]
     FileDirPath.append(fpath)
 
     i = 0
@@ -28,9 +31,9 @@ def GetOneDirFile(fpath):
     dirs = os.listdir(fpath)
     # 開檔案
     # with open('filename.csv', 'w') as Mycsv: # 新檔案寫入模式
-    with open('filename.csv', 'a') as Mycsv:  # 舊檔案增加模式 append mode
+    with open('filename.csv', 'a' , encoding = "utf-8") as Mycsv:  # 舊檔案增加模式 append mode
         # 拿筆
-        MyPen = csv.writer(Mycsv , delimiter='\t')
+        MyPen = csv.writer(Mycsv , delimiter='\t' )
         # 先空一行壓壓驚
         MyPen.writerow([])
         # 寫時間
@@ -39,7 +42,7 @@ def GetOneDirFile(fpath):
 
         for names in dirs:
             # 全檔名
-            tempFullPath = fpath + '/' + names
+            tempFullPath = fpath + os.sep + names
 
             # 跳過列表的檔案
             if names in ExFile: continue
@@ -60,12 +63,22 @@ def GetOneDirFile(fpath):
                 subfilename += "." + temp[i]
                 i += 1
 
+            fsize = translateSize(os.path.getsize(tempFullPath))
             # 印出主檔名 副檔名 檔案大小
-            MyPen.writerow(["",temp[0], subfilename, os.path.getsize(tempFullPath)])
+            MyPen.writerow(["",temp[0], subfilename, fsize[0], sizeLevel[fsize[1]] ])
 
         # 隨手關檔好習慣
         Mycsv.flush()
         Mycsv.close()
+
+def translateSize(fsize, lv = 0):
+    # lv 0 -> Byte
+    # lv 1 -> kB
+    # lv 2 -> MB
+    # lv 3 -> GB
+    # lv 4 -> TB
+    if fsize < 1024: return (fsize , lv)
+    else: return translateSize(int(fsize/1024) , lv+1)
 
 if __name__ == '__main__':
     main()
